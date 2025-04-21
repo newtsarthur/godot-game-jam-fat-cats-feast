96d4e5ef4a6ec6e10db058d3b084e9434da3c9f8
newtsarthur <tutuhoficial13@gmail.com>
2025-04-20 21:06:22 -0300

    PlayerData Script added and Player collect item

--- scripts/Balance.cs
+++ scripts/Balance.cs
@@ -1,13 +1,14 @@
 using Godot;
 using System;
 
-public partial class Balance : Sprite2D
+public partial class Balance : Area2D
 {
     private Tween blinkTween;
 
     public override void _Ready()
     {
         StartBlinkIdle();
+        BodyEntered += OnBodyEntered;
     }
 
     public void StartBlinkIdle()
@@ -40,5 +41,17 @@ public partial class Balance : Sprite2D
         {
             shaderMaterial.SetShaderParameter("blink_intensity", value);
         }
+    }
+    private void OnBodyEntered(Node body)
+    {
+        if(body.IsInGroup("PlayerGroup"))
+        {
+            AddPointPlayer();
+            QueueFree();
+        }
+    }
+    public void AddPointPlayer() 
+    {
+        PlayerData.Instance.ItemsCollected++;
     }
 }
--- scripts/PlayerData.cs
+++ scripts/PlayerData.cs
@@ -3,6 +3,13 @@ using System;
 
 public partial class PlayerData : Node
 {
-  public int DeathCount = 0;
-  public int ItemsCollected = 0;
+    public static PlayerData Instance { get; private set; }
+
+    public int DeathCount = 0;
+    public int ItemsCollected = 0;
+
+    public override void _Ready()
+    {
+        Instance = this;
+    }
 }
